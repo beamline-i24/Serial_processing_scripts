c4cb7caa952cee0fc5f96c49ec87f82dd6141b18
Appleby <web66492@i24-ws006.diamond.ac.uk>
2018-06-21 17:12:53 +0100

    added merging/merging_data_maker get_int.py

---
+++ merging/merging_dat_maker/get_int.sh
@@ -0,0 +1,18 @@
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_1x -uc True -uct Small 
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_1x -uc True -uct Big
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_2x -uc True -uct Small 
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_2x -uc True -uct Big
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_4x -uc True -uct Small 
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_4x -uc True -uct Big
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_8x -uc True -uct Small 
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_8x -uc True -uct Big
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_10x -uc True -uct Small 
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_10x -uc True -uct Big
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_15x -uc True -uct Small 
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_15x -uc True -uct Big
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_20x -uc True -uct Small 
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_20x -uc True -uct Big
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_30x -uc True -uct Small 
+python get_int.py -s /dls/i24/data/2017/nt14493-63/processing/stills_process/bender/redo_30x -uc True -uct Big
+
+
